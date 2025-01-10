7c29f4f1592987d8058cc171d68d804e34ab3279
billchetch <45006134+billchetch@users.noreply.github.com>
2025-01-10 22:42:52 +0700

    sdfdsf

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,7 +15,7 @@ class Program
     //a comment
     public class TestDbContext : ChetchDbContext
     {
-        public DbSet<Test> Tests { get; set; }
+        public DbSet<Test> tests { get; set; }
 
         public TestDbContext() : base("test"){}
     }   
@@ -29,15 +29,13 @@ class Program
         ChetchDbContext.Config = settings;
 
         using(var tdbc = new TestDbContext()){
-            var test = tdbc.Tests;
-            foreach(var t in tdbc.Tests){
+
+            foreach(var t in tdbc.tests){
                 Console.WriteLine("ID {0}, test: {1}", t.ID, t.SomeField);
             }
         }
+
         
-        Console.Write("Hello, World!  Press a key to continue");
-        var ki = Console.ReadKey(true);
-        Console.WriteLine("\nok so you've pressed: {0}", ki.KeyChar);
         Console.Write("Press any key to end...");
         Console.ReadKey(true);
     }
